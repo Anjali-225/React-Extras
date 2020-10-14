204cd7fc97cd821e88ab46cdc105fee90612f220
Anjali <anjali.payal69@gmail.com>
2020-10-14 18:42:28 +0200

    The Context API & useContext Hooks Part 2

--- react-new-features/src/components/NoteApp.js
+++ react-new-features/src/components/NoteApp.js
@@ -2,14 +2,11 @@ import React, { useEffect, useReducer } from 'react';
 import notesReducer from '../reducers/notes'
 import NoteList from './NoteList'
 import AddNoteForm from './AddNoteForm'
+import NotesContext from '../context/notes-context'
 
 const NoteApp = () => {
     const [notes, dispatch] = useReducer(notesReducer, [])
-
-    const removeNote = (title) => {
-        dispatch({ type: 'REMOVE_NOTES', title })
-    }
-
+    
     useEffect(() => {
         const notes = JSON.parse(localStorage.getItem('notes'))
         if(notes){
@@ -22,12 +19,11 @@ const NoteApp = () => {
     }, [notes])
 
     return (
-        <div>
+        <NotesContext.Provider value={{ notes, dispatch }}>
             <h1>Notes</h1>
-            <NoteList notes = {notes} removeNote = {removeNote} />
-            <AddNoteForm dispatch={dispatch} />
-        </div>
-
+            <NoteList />
+            <AddNoteForm />
+        </NotesContext.Provider>
     )
 }
 
